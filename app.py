89643a28a73d14ab0bba386d57c1f85f11e04b8d
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-14 08:19:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -46,30 +46,30 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  try:
+    try:
+        instruction = "子供でも理解できる簡単な言葉で答えてください。漢字にはふりがなを付けてください。"
 
-instruction = "子供でも理解できる簡単な言葉で答えてください。漢字にはふりがなを付けてください。"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0301",
+            messages=[
+                {"role": "system", "content": "子供でも理解できる簡単な言葉で答えてください。漢字にはふりがなを付けてください。"},
+                {"role": "user", "content": event.message.text + " " + instruction}
+            ]
+        )
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo-0301",
-  messages=[
-    {"role": "system", "content": "子供でも理解できる簡単な言葉で答えてください。漢字にはふりがなを付けてください。"},
-    {"role": "user", "content": event.message.text + " " + instruction}
-  ]
-)
+        # AIからの応答を取得する
+        print(response['choices'][0]['message']['content'])
 
-    # AIからの応答を取得する
-    print(response['choices'][0]['message']['content'])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=response['choices'][0]['message']['content']),
+        )
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text = response['choices'][0]['message']['content']),
-    )
-
-  except InvalidRequestError as e:
-    print(e)
-    print(type(e))
+    except InvalidRequestError as e:
+        print(e)
+        print(type(e))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
