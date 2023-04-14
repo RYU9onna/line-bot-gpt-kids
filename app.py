aa7dc4430ffa71d7c2d80c90adb0084f491394d1
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-14 09:24:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -49,11 +49,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
+        instruction = "簡単な言葉と全ての漢字にふりがなを使って子供にもわかるように答えてください。"
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0301",
             messages=[
-                {"role": "system", "content": "あなたはひらがなだけで答えます。"},
-                {"role": "user", "content": event.message.text + "\nひらがなだけ使って。"}
+                {"role": "system", "content": instruction},
+                {"role": "user", "content": "ひらがなで答えて。\n" + event.message.text + "\nひらがなで答えて。\nわかりやすく教えて\nひらがなで答えて。"}
             ]
         )
 
